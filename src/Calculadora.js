6e4633994ce7f30674bece48149d6034cf445646
Isabelle Vimercati <103614557+BelleVimercati@users.noreply.github.com>
2024-06-27 15:01:53 -0300

    feat: novas funcionalidades e testes

--- src/Calculadora.js
+++ src/Calculadora.js
@@ -21,5 +21,27 @@ export default class Calculadora{
             throw new Error("Não é possível calcular a raiz quadrada de um número negativo.");
         }
         return Math.sqrt(a);
+    } 
+
+    static cachorroPraHumano(idadeCachorro){
+         if(idadeCachorro < 1){
+            let anosCachorro = (1/12) * (idadeCachorro * 10);
+            return Math.ceil(16 * Math.log(anosCachorro) + 31)
+        }
+        else 
+            return Math.ceil(16 * Math.log(idadeCachorro) + 31)
+    }
+    
+    static diasEntre(dataEscolhida){
+        const hoje = new Date()
+        const data = new Date(dataEscolhida)
+        const diferencaTempo = hoje - data;
+        const diferencaDias = Math.floor(diferencaTempo / (86400000));
+
+        if(diferencaDias < 0){
+            return diferencaDias * (-1);
+        }
+        else
+            return diferencaDias;
     }
 }
--- test/calculadora.spec.js
+++ test/calculadora.spec.js
@@ -1,4 +1,3 @@
-import assert from "assert";
 import { expect } from "chai";
 import Calculadora from "../src/Calculadora.js";
 
@@ -48,7 +47,7 @@ describe("Testes de Multiplicação", () => {
   });
 });
 
-describe("Testes de Percentual", () => {
+ describe("Testes de Percentual", () => {
   it("Deve calcular 10% de 500", () => {
     let resultado = Calculadora.porcentagem(10, 500);
     expect(resultado).to.be.eq(50);
@@ -73,5 +72,36 @@ describe("Testes de Raiz Quadrada", () => {
 
   it("Deve calcular a raiz quadrada de -25", () => {
     expect(() => Calculadora.raizQuadrada(-25)).to.throw("Não é possível calcular a raiz quadrada de um número negativo.");
+  });
+}); 
+
+describe("Testes de Idade de cachorro", () => {
+  it("Deve calcular a idade de um cachorro de 1 ano", () => {
+    let resultado = Calculadora.cachorroPraHumano(1);
+    expect(resultado).to.be.eq(31);
+  });
+
+  it("Deve calcular a idade de um cachorro de 3 meses", () => {
+    let resultado = Calculadora.cachorroPraHumano(0.3);
+    expect(resultado).to.be.eq(9);
+  });
+  it("Deve calcular a idade de um cachorro de 12 anos", () => {
+    let resultado = Calculadora.cachorroPraHumano(12);
+    expect(resultado).to.be.eq(71);
+  });
+});
+
+describe("Testes cálculo de dias", () => {
+  it("Deve calcular a quantidade de dias corridos desde 25 de junho de 2000", () => {
+    let resultado = Calculadora.diasEntre("2000-06-25");
+    expect(resultado).to.be.eq(8768);
+  });
+  it("Deve calcular a quantidade de dias corridos desde 25 de março de 1998", () => {
+    let resultado = Calculadora.diasEntre("1998-03-25");
+    expect(resultado).to.be.eq(9591);
+  });
+  it("Deve calcular a quantidade de dias até 25 de março de 2030", () => {
+    let resultado = Calculadora.diasEntre("2030-03-25");
+    expect(resultado).to.be.eq(2097);
   });
 });
